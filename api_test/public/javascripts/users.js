3361823fe897e521fc96bba55dc409a25dacb127
Alex Beaman <dabeamanator@gmail.com>
2015-07-11 10:44:52 -0500

    All the debuggers

---
+++ api_test/public/javascripts/users.js
@@ -0,0 +1,57 @@
+var app = angular.module('accountCreate', []);
+
+app.controller('accountCreateController', function($scope, $http){
+
+    $scope.formData ={};
+    $scope.eventData ={};
+    $scope.userData = {};
+
+    $http.get('/api/users')
+        .success(function(data){
+            $scope.eventData = data;
+            console.log(data);
+        })
+        .error(function(error){
+            console.log(error);
+        });
+
+    $scope.createUser = function() {
+        debugger;
+        // user variable is dynamically built by
+        //  angular through HTML page [index.html]
+        $http.post('/api/users', $scope.user)
+            .success(function(data) {
+                debugger;
+                console.log(data);
+            })
+            .error(function(error) {
+                debugger
+                console.log('Error: ' + error);
+            })
+
+        debugger;
+        $http.post('/users/api/users', $scope.user)
+            .success(function(data) {
+                debugger;
+                console.log(data);
+            })
+            .error(function(error) {
+                debugger
+                console.log('Error: ' + error);
+            })
+    };
+    
+    $scope.deleteUser = function(uid){
+        debugger;
+        $http.delete('/api/users/' + uid)
+            .success(function(data) {
+                debugger;
+                $scope.userData = data;
+                console.log(data);
+            })
+            .error(function(error) {
+                debugger
+                console.log('Error: ' + error);
+            });
+    };
+});
--- api_test/routes/users.js
+++ api_test/routes/users.js
@@ -2,8 +2,48 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    console.log("inside users.js route");
+    debugger;
+    //res.send('respond with a resource');
+});
+
+router.get('/api/users', function(req,res){
+	debugger;
+	return "hi"; // possibly should return object / array of objects w/ user data?
+});
+
+router.post('/api/users', function(req, res) {
+    var results = [];
+    console.log(req.body);
+	with(req.body){
+	    var data = [
+			Email,
+			Password,
+			"First"
+	    ];
+	    return data;
+    }
+});
+
+router.post('/users/api/users', function(req, res) {
+    var results = [];
+    console.log(req.body);
+	with(req.body){
+	    var data = [
+			Email,
+			Password,
+			"Second"
+	    ];
+	    return data;
+    }
+});
+
+router.delete('/api/users/:uid', function(req, res) {
+	console.log("delete user");
+	debugger;
+	return "success?";
 });
 
 module.exports = router;
