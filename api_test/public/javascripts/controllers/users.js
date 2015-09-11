2ac728c059899fbe2fa64720b99385ce37563c98
Alex Beaman <dabeamanator@gmail.com>
2015-09-10 22:17:23 -0500

    connect submit button (not done)

--- api_test/public/javascripts/controllers/users.js
+++ api_test/public/javascripts/controllers/users.js
@@ -1,9 +1,10 @@
 /*App & Controller for initial_signup html*/
 
-angular.module('scoober').controller('accountCreateController', function($scope, $http){
+angular.module('scoober').controller('accountCreateController', function($scope, $http, $userParams, $location){
 
     $scope.userData = {};
 
+    // get all users just to display for testing.
     $http.get('/api/users')
         .success(function(data){
             $scope.userData = data;
@@ -23,6 +24,8 @@ angular.module('scoober').controller('accountCreateController', function($scope,
             .success(function(data) {
                 $scope.userData = data;
                 console.log('added ' + data + ' to users table');
+                // serve up new_profile page to give user the chance to fill in demographics
+                $location.path('/newprofile');///' + eid);
             })
             .error(function(error) {
                 console.log('Error: ' + error);
