bc40f71559a1b0ba4101e583c54d1e13cfad6a4c
Alex Beaman <dabeamanator@gmail.com>
2015-10-07 23:43:44 -0500

    hopefully fixed group_type

--- api_test/routes/api.js
+++ api_test/routes/api.js
@@ -349,8 +349,8 @@ router.get('/groups', function(req,res){
 
 // Add to groups page, return all groups
 router.post('/groups', function(req, res) {
-        var results = [];
-        console.log(req.body);
+    var results = [];
+    console.log(req.body);
     with(req.body){
         var data = [
             name,
@@ -366,7 +366,7 @@ router.post('/groups', function(req, res) {
     console.log(data);
     pg.connect(connectionString, function(err, client, done) {
 
-        client.query("INSERT INTO groups2(name, security, location, group_type, details, min_players, max_players, group_picture) values($1,$2,$3,$4,$5,$6,$7,$8)", data);
+        client.query("INSERT INTO groups2(name, security, location, type, details, min_players, max_players, group_picture) values($1,$2,$3,$4,$5,$6,$7,$8)", data);
 
         var query = client.query("SELECT * FROM groups2 ORDER BY gid ASC");
 
